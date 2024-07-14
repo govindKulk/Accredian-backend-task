f15631b8799b0330b5f7a88072c9012cc036a6d0
govindKulk <kulkarnigovind2003@gmail.com>
2024-07-15 02:43:10 +0530

    added postinstall

---
+++ prisma/migrations/20240714210607_test/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Test` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
